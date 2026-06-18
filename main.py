aafb18bfb81e255160f8f0d9a0a1ea4440d031b8
Priya Deepika <ppdeepu.123@gmail.com>
2026-06-18 16:07:42 +0530

    Added document chunking and metadata

--- main.py
+++ main.py
@@ -1,6 +1,22 @@
 from langchain_community.document_loaders import PyPDFLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+
 loader = PyPDFLoader("research_papers/paper1.pdf")
 docs = loader.load()
 print(f"Total number of pages: {len(docs)}")
-print('First Page: \n\n')
-print(docs[0].page_content[:1000])
+
+splitter = RecursiveCharacterTextSplitter(
+    chunk_size = 500,
+    chunk_overlap=100
+)
+chunks = splitter.split_documents(docs)
+
+print("Total Chunks: ", len(chunks))
+print("First Chunk:\n")
+print(chunks[0].page_content)
+
+print("\nSecond Chunk:\n")
+print(chunks[1].page_content)
+
+print('\n\n')
+print(chunks[0].metadata)
