e43ec453ce02994c289272891a753eae92e670d2
Priya Deepika <ppdeepu.123@gmail.com>
2026-06-18 16:25:26 +0530

    Added Gemini Embeddings

--- main.py
+++ main.py
@@ -1,22 +1,38 @@
 from langchain_community.document_loaders import PyPDFLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 
-loader = PyPDFLoader("research_papers/paper1.pdf")
-docs = loader.load()
-print(f"Total number of pages: {len(docs)}")
+from dotenv import load_dotenv
+from langchain_google_genai import GoogleGenerativeAIEmbeddings
+load_dotenv()
 
-splitter = RecursiveCharacterTextSplitter(
-    chunk_size = 500,
-    chunk_overlap=100
+
+# loader = PyPDFLoader("research_papers/paper1.pdf")
+# docs = loader.load()
+# print(f"Total number of pages: {len(docs)}")
+
+# splitter = RecursiveCharacterTextSplitter(
+#     chunk_size = 500,
+#     chunk_overlap=100
+# )
+# chunks = splitter.split_documents(docs)
+
+# print("Total Chunks: ", len(chunks))
+# print("First Chunk:\n")
+# print(chunks[0].page_content)
+
+# print("\nSecond Chunk:\n")
+# print(chunks[1].page_content)
+
+# print('\n\n')
+# print(chunks[0].metadata)
+
+embeddings = GoogleGenerativeAIEmbeddings(
+    model = "gemini-embedding-001"
 )
-chunks = splitter.split_documents(docs)
 
-print("Total Chunks: ", len(chunks))
-print("First Chunk:\n")
-print(chunks[0].page_content)
+vector = embeddings.embed_query(
+    "What is Artificial Intelligence?"
+)
 
-print("\nSecond Chunk:\n")
-print(chunks[1].page_content)
-
-print('\n\n')
-print(chunks[0].metadata)
+print("Vector: ", vector[:10])
+print(len(vector))
